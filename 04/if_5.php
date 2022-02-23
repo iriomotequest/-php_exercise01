9bb1ec02870ab29554a049311721998c31366ec6
iriomotequest <iriomotenashi@gmail.com>
2022-02-23 11:19:47 +0900

    制御構文1の課題

--- 04/if_5.php
+++ 04/if_5.php
@@ -1,11 +1,18 @@
 <?php
 
-$num = 3434;
+$num = 1;
 
-if ($num == 1 || $num % 2 == 0 || $num % 3 == 0 || $num % 5 == 0 || $num % 7 == 0) {
+if ($num == 1) {
     echo "{$num}は素数ではありません";
-} elseif ($num % 2 == 0 || $num % 3 == 0 || $num % 5 == 0 || $num % 7 == 0 && $num > 10 || $num % $num == 0) {
-    echo "{$num}は素数です";
-} else {
-    echo "{$num}は素数です";
 }
+
+for ($i = 2; $i < $num; $i++) {
+    if ($num % $i == 0) {
+        echo "{$num}は素数ではありません";
+    } else {
+        echo "{$num}は素数です";
+    }
+    break;
+}
+
+
