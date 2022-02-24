96990249b1fc95320c0e007537cd3b3a61c325f5
iriomotequest <iriomotenashi@gmail.com>
2022-02-24 18:14:07 +0900

    制御構文２修正致しました

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -5,7 +5,7 @@ $fund_raising = 101;
 
 echo "あなたの所持金は{$poket_money}です。<br>";
 
-while ($poket_money > $fund_raising) {
+while ($poket_money >= $fund_raising) {
     echo "{$fund_raising}円募金しました。<br>";
     $poket_money -= $fund_raising;
     echo "残り残金は{$poket_money}です。<br>";
--- 05/if2_2.php
+++ 05/if2_2.php
@@ -8,6 +8,7 @@ echo "あなたの所持金は{$poket_money}です。<br>";
 do {
     echo "{$fund_raising}円募金しました。<br>";
     $poket_money -= $fund_raising;
-} while ($poket_money > 0);
+    echo "残り残金は{$poket_money}です。<br>";
+} while ($poket_money >= $fund_raising);
 
-echo "残り残金は{$poket_money}です。<br>";
+echo 'あなたはこれ以上募金できません';
