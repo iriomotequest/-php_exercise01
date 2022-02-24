adb8b7856ad30874ad73c4a7527262b7404dd871
iriomotequest <iriomotenashi@gmail.com>
2022-02-24 15:16:09 +0900

    制御構文２修正致しました

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -3,14 +3,11 @@
 $poket_money = 1000;
 $fund_raising = 101;
 
-while ($poket_money > 0) {
-    echo "あなたの所持金は{$poket_money}です。<br>
-    {$fund_raising}円募金しました。<br>
-    残り残金は{$poket_money}です。<br>";
-    $poket_money -= $fund_raising;
-    if($poket_money < 0) {
-    echo 'あなたはこれ以上募金できません';
-    return 0;
-    }
-}
+echo "あなたの所持金は{$poket_money}です。<br>";
 
+while ($poket_money > $fund_raising) {
+    echo "{$fund_raising}円募金しました。<br>";
+    $poket_money -= $fund_raising;
+    echo "残り残金は{$poket_money}です。<br>";
+}
+echo 'あなたはこれ以上募金できません';
--- 05/if2_2.php
+++ 05/if2_2.php
@@ -3,13 +3,11 @@
 $poket_money = 1000;
 $fund_raising = 101;
 
+echo "あなたの所持金は{$poket_money}です。<br>";
+
 do {
-    echo "あなたの所持金は{$poket_money}です。<br>
-    {$fund_raising}円募金しました。<br>
-    残り残金は{$poket_money}です。<br>";
+    echo "{$fund_raising}円募金しました。<br>";
     $poket_money -= $fund_raising;
 } while ($poket_money > 0);
-if ($poket_money < 0) {
-    echo 'あなたはこれ以上募金できません';
-    return 0;
-}
+
+echo "残り残金は{$poket_money}です。<br>";
