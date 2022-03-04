abcc974e59a484110ee74290ed23dbcc8419759b
iriomotequest <iriomotenashi@gmail.com>
2022-03-04 14:59:53 +0900

    関数の課題訂正

--- 08/function_5.php
+++ 08/function_5.php
@@ -1,14 +1,19 @@
 <?php
 
-//変数定義
-$time = '';
-$msg = '';
-$week = ['日', '月', '火', '水', '木', '金', '土'];
-$day = date('w');
+$week = [
+    '日',
+    '月',
+    '火',
+    '水',
+    '木',
+    '金',
+    '土',
+];
 
-//変数time受け取り
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $time = $_POST['time'];
+    $time = date('Y年m月d日');
+    $oneWeek = $week[date('w')];
+    $msg = $time . $oneWeek;
 }
 ?>
 
@@ -19,32 +24,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>function_5</title>
 </head>
 
 <body>
-    <!--first message-->
-    <?php if (empty($time)) : ?>
-        <h2><?= $msg = "本日の日付、曜日を確認しますか？" ?></h2>
-    <?php endif; ?>
-
-    <!--はいbtn-->
-    <?php if (!empty($msg)) : ?>
+    <? if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
+        <h1><?= '今日は、' . $msg . '曜日です'?></h1>
+        <div>
+            <a href="function_5.php">戻る</a>
+        </div>
+    <? else : ?>
+        <h2>本日の日付、曜日を確認しますか？</h2>
         <form action="" method="POST">
-            <input type="submit" name="time" value="はい">
+            <input type="submit" value="はい">
         </form>
-    <?php endif; ?>
-
-    <!--time messge-->
-    <?php if ($time) : ?>
-        <h2><?= $time = '今日は、' . date('Y年m月d日') . $week[$day] . '曜日です<br>'; ?></h2>
-    <?php endif; ?>
-
-    <!--戻るbtn-->
-    <?php if ($time) : ?>
-        <a href="">戻る</a>
-    <?php endif; ?>
-
+    <? endif; ?>
 </body>
 
-</html>
+</html> 
